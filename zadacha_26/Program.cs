40be99d29d26a6dbcd6d2c3910e0f02af49ff6f6
Larina <Losiknor@gmail.com>
2022-02-10 15:05:34 +0700

    Задача 27 недоделанная

--- zadacha_26/Program.cs
+++ zadacha_26/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// № 25 Найти сумму чисел от 1 до А
+int numberA = new Random().Next(1, 100);
+int sum = 0;
+int i = 1;
+while (i <= numberA)
+{
+    sum = sum + numberA;
+    i++;
+}
+Console.WriteLine("Сумма чисел от 1 до " + numberA + "   равна   " + sum);
+
+int a = 100;
+int sum = (a*(a+1))/2;
+Console.WriteLine(sum);
---
+++ zadacha_27/Program.cs
@@ -0,0 +1,17 @@
+﻿// № 27 Определить количество цифр в числе
+Console.WriteLine("Введите число: ");
+int digitCount (int a);
+{
+int counter = 0;
+while (a > 0)
+{
+    counter++;
+    a = a / 10;
+}
+return counter;
+}
+string str = Console.ReadLine();
+
+int x = Convert.ToInt32(str)4;
+
+Console.WriteLine( "Количество цифр в числе" + digitCount (x) );
